84249e43b4b67dcc89c96c18f94a00b367af237d
dylant1 <dylant1@yahoo.com>
2023-01-03 14:20:47 -0600

    Add clap for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use reqwest;
+use clap::Parser;
 use std::env;
-use mini_redis::{client, Result};
+use mini_redis::Result;
+
+#[derive(Parser)]
+struct Cli {
+    cmd: String,
+}
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
 
+    let args = Cli::parse();
+
+    //for _ in 0..args.count { 
+        //println!("hello: {}", args.cmd);
+    //}
+    println!("COMMAND: {} ", args.cmd);
+
     let access_token: &'static str = env!("ACCESS_TOKEN");
     let url ="https://canvas.instructure.com/api/v1/courses?access_token=".to_owned() + access_token;
+
 
     let body = reqwest::get(url).await?.text().await?;
 
@@ -14,4 +29,8 @@ async fn main() -> Result<()> {
     println!("TOKEN: {}", access_token);
     
     Ok(())
+}
+
+async fn get_grades() {
+
 }
